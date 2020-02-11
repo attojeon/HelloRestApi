792ceb99a1e129a01ba335b64bee9d21d51fea9a
atto.jeon@gmail.com <atto.jeon@gmail.com>
2020-02-11 15:18:03 +0900

    Add project files.

---
+++ HelloRestApi/Controllers/PeopleController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloRestApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        // GET: api/People
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/People/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // GET: api/People/5
+        [HttpGet("{min}/{max}", Name = "Get")]
+        public string Get(int min, int max)
+        {
+            return "value_" + ( min*max ).ToString();
+        }
+
+        // POST: api/People
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/People/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
